cf21510253826f9e65d2c986cb7e870082b9f06c
Ryan Burnside <pixeloutlaw@gmail.com>
2022-08-13 21:13:53 -0600

    Update camo.scm

--- camo.scm
+++ camo.scm
@@ -10,7 +10,7 @@
  ;; Widgets and top level parameters
  SF-IMAGE      "Image"            0
  SF-DRAWABLE   "Drawable"         0
- SF-COLOR      "Top Color"        '(3 22 2) ; There is some strangeness here. Seems to be drawn transparent!?
+ SF-COLOR      "Top Color"        '(3 22 2)
  SF-COLOR      "Under Color"      '(144 142 44)
  SF-COLOR      "Under Color 2"    '(134 103 6)
  SF-COLOR      "Background Color" '(59 83 9)
@@ -20,63 +20,47 @@
 (script-fu-menu-register "script-fu-make-camo"
                          "<Image>/Script-Fu/")
 
+;; Accessory function for script-fu-make-camo 
+(define (add-camo-layer image color roughness threshold name)
+  (let* ((width (car (gimp-image-width image)))
+         (height (car (gimp-image-height image)))
+         (new-layer (car (gimp-layer-new image width height 1 name 100 LAYER-MODE-NORMAL-LEGACY))))
+    (gimp-context-push)
+    (gimp-context-set-defaults)
+    (gimp-image-add-layer image new-layer 0)
+    (gimp-selection-none image)
+    (plug-in-solid-noise 1 image new-layer 1 0 (random 65536) roughness 4.1 4.1)
+    (gimp-drawable-threshold new-layer 0 threshold 1.0)
+    (gimp-context-set-foreground color)
+    (gimp-image-select-color image CHANNEL-OP-ADD new-layer '(255 255 255))
+    (gimp-edit-bucket-fill new-layer BUCKET-FILL-FG LAYER-MODE-NORMAL-LEGACY 100 0 FALSE 0 0)
+    (gimp-selection-invert image)
+    (gimp-edit-cut new-layer)
+    (gimp-context-pop)))
+
 ;; Main function
 (define (script-fu-make-camo image drawable top-color under-color under-color2 background-color roughness)
   (let* ((width (car (gimp-image-width image)))
          (height (car (gimp-image-height image)))
-         (top-layer (car (gimp-layer-new image width height 1 "Top Camo" 100 LAYER-MODE-NORMAL-LEGACY)))
-         (under-layer (car (gimp-layer-new image width height 1 "Under Camo" 100 LAYER-MODE-NORMAL-LEGACY)))
-         (under-layer2 (car (gimp-layer-new image width height 1 "Under Camo 2" 100 LAYER-MODE-NORMAL-LEGACY)))
-         (background-layer (car (gimp-layer-new image width height 1 "Background Camo" 100 LAYER-MODE-NORMAL-LEGACY)))
-         (top-r (car top-color))
-         (top-g (cadr top-color))
-         (top-b (caddr top-color))
-         (under-r (car under-color))
-         (under-g (cadr under-color))
-         (under-b (caddr under-color))
-         (under2-r (car under-color2))
-         (under2-g (cadr under-color2))
-         (under2-b (caddr under-color2)))
+         (background-layer (car (gimp-layer-new image width height 1 "Camo 4" 100 LAYER-MODE-NORMAL-LEGACY))))
 
     (gimp-image-undo-group-start image)
     (gimp-context-push)
     (gimp-context-set-defaults)
-    
+
     ;; Add the 3 layers
     (gimp-image-add-layer image background-layer 0)
-    (gimp-image-add-layer image under-layer2 0)
-    (gimp-image-add-layer image under-layer 0)
-    (gimp-image-add-layer image top-layer 0)
 
     ;; Set the backing background color
     (gimp-selection-none image)
     (gimp-context-set-foreground background-color)
     (gimp-drawable-fill background-layer FILL-FOREGROUND)
 
-    ;; Top Layer (small flecks of color)
-    (gimp-selection-none image)
-    (plug-in-solid-noise 1 image top-layer 1 0 (random 65536) roughness 4.1 4.1)
-    (gimp-drawable-threshold top-layer 0 0.6 1.0)
-    (plug-in-exchange 1 image top-layer 255 255 255 top-r top-g top-b 0 0 0)
-    (gimp-image-select-color image CHANNEL-OP-REPLACE top-layer '(0 0 0))
-    (gimp-edit-cut top-layer)
-
-    ;; Under Layer (larger blocks of color)
-    (gimp-selection-none image)
-    (plug-in-solid-noise 1 image under-layer 1 0 (random 65536) roughness 4.1 4.1)
-    (gimp-drawable-threshold under-layer 0 0.55 1.0)
-    (plug-in-exchange 1 image under-layer 255 255 255 under-r under-g under-b 0 0 0)
-    (gimp-image-select-color image CHANNEL-OP-REPLACE under-layer '(0 0 0))
-    (gimp-edit-cut under-layer)
-
-    ;; Under Layer2 (larger blocks of color)
-    (gimp-selection-none image)
-    (plug-in-solid-noise 1 image under-layer2 1 0 (random 65536) roughness 4.1 4.1)
-    (gimp-drawable-threshold under-layer2 0 0.55 1.0)
-    (plug-in-exchange 1 image under-layer2 255 255 255 under2-r under2-g under2-b 0 0 0)
-    (gimp-image-select-color image CHANNEL-OP-REPLACE under-layer2 '(0 0 0))
-    (gimp-edit-cut under-layer2)
-        
+    ;; Add our 3 camo layers
+    (add-camo-layer image under-color2 roughness 0.5 "Camo 2")
+    (add-camo-layer image under-color roughness 0.5 "Camo 1")
+    (add-camo-layer image top-color roughness 0.55 "Camo 0")
+    
     (gimp-displays-flush)
     (gimp-context-pop)
     (gimp-image-undo-group-end image)))
